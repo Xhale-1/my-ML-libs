bded13be7194d8bf4d87911729f40145b74c52b6
Mikhial Rud <142402709+Xhale-1@users.noreply.github.com>
2025-05-18 19:06:58 +0300

    Update FCNN_pipeline.py

--- Pipelines/FCNN_pipeline.py
+++ Pipelines/FCNN_pipeline.py
@@ -286,45 +286,55 @@ def save_to_drive(model,name, path):
 #_____________________________________________
 
 
-def predict(model,loader, device, yss=0):
+def predict(model,loaders, device, yss=0):
   model.eval()
   preds = []
   ys = []
   out = []
-  with torch.no_grad():
-    for batch in loader:
-      pred = model(batch[0].to(device))
-      preds.append(pred.cpu())
-      if yss:
-        ys.append(batch[1])
-  preds = torch.cat((preds),0)
-  out.append(preds)
-  if yss:
-    ys = torch.cat((ys),0)
-    out.append(yss)
-  return out
+  out_y = []
+  for loader in loaders:
+    with torch.no_grad():
+      for batch in loader:
+        pred = model(batch[0].to(device))
+        preds.append(pred.cpu())
+        if yss:
+          ys.append(batch[1])
+    preds = torch.cat((preds),0)
+    out.append(preds)
+    if yss:
+      ys = torch.cat((ys),0)
+      out_y.append(yss)
+
+  return out, out_y
 
 
 
-def inference(model, trloader0, y, device, scaler2 = 0, print_loss = 1):
-  [preds_tr] = predict(model,trloader0, device)
+def inference(model, loaders, ys, device, scaler2 = 0, print_loss = 1):
+  
+  data = zip(loaders,ys)
+  for i,(loader,y) in enumerate(data):
+    [preds_tr], _ = predict(model,[loader], device)
 
-  if isinstance(y, torch.Tensor):
-    y = y.detach().numpy()
-  else:
-    y = np.array(y)
+    if isinstance(y, torch.Tensor):
+      y = y.detach().numpy()
+    else:
+      y = np.array(y)
 
-  preds1_tr = preds_tr.detach().numpy()
-  y1_tr = y
-  if isinstance(scaler2, StandardScaler):
-    preds1_tr = scaler2.inverse_transform(preds1_tr)
-    y1_tr = scaler2.inverse_transform(y)
+    preds1_tr = preds_tr.detach().numpy()
+    y1_tr = y
+    if isinstance(scaler2, StandardScaler):
+      preds1_tr = scaler2.inverse_transform(preds1_tr)
+      y1_tr = scaler2.inverse_transform(y)
 
-  if print_loss:
-    print(np.array(list(zip(preds1_tr[:5],y1_tr[:5]))).reshape(-1,2))
+    if print_loss:
+      print(np.array(list(zip(preds1_tr[:5],y1_tr[:5]))).reshape(-1,2))
 
-
-  rmse0 = root_mean_squared_error(y1_tr, preds1_tr)
-  if print_loss:
-    print(f'rmse test: {rmse0}')
+    rmse0 = root_mean_squared_error(y1_tr, preds1_tr)
+    if print_loss:
+      print(f'rmse test: {rmse0}')
+    
+    relerr = ( np.abs(y1_tr -  preds1_tr) / y1_tr ) *100
+    maxrelerr = relerr.max()
+    print(f'макс относительная ошибка (%) = {maxrelerr} ({i})')
+    
   return preds_tr, preds1_tr, rmse0
